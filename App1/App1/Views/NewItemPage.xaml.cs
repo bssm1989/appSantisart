51be5fa9a982c62c1ef20d13ad3bede5d33b7014
bssm1989 <bssm1989@gamil.com>
2019-09-27 17:14:14 +0700

    yt

--- App1/App1/Views/NewItemPage.xaml.cs
+++ App1/App1/Views/NewItemPage.xaml.cs
@@ -5,6 +5,10 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using App1.Models;
+using SQLite;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace App1.Views
 {
@@ -26,8 +30,47 @@ namespace App1.Views
             };
 
             BindingContext = this;
-        }
+            getPerson();
 
+        }
+        public static List<SelectableData<ExampleData>> SelectedData { get; set; }
+        private async void getPerson()
+        {
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Person>();
+            HttpClient client = new HttpClient();
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+            var httpClient = new HttpClient(new System.Net.Http.HttpClientHandler());
+            var response = await httpClient.GetStringAsync("https://admin.santisartwitt.com/Employees/indexListJson");
+            var products = JsonConvert.DeserializeObject<List<Person>>(response);
+            //SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Person>();
+            foreach (var item in products)
+            {
+
+                conn.Insert(item);
+            }
+            var posts = conn.Table<Person>().ToList();
+            if (posts != null)
+            {
+                SelectedData = new List<SelectableData<ExampleData>>();
+                foreach (var item in posts)
+                {
+                    SelectedData.Add(new SelectableData<ExampleData>()
+                    {
+                        Data = new ExampleData() { Name = item.EmpName, Description = item.EmpName }
+                    });
+                }
+            }
+
+
+
+
+
+
+
+            conn.Close();
+        }
         async void Save_Clicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "AddItem", Item);
